9772c973ad50266e1d0e06e645854e7c9b41ebcf
SMADITYA05 <smadityasm@gmail.com>
2025-07-02 20:21:19 +0530

    Create Popcount255.v

---
+++ more verilog features/Popcount255.v
@@ -0,0 +1,10 @@
+module top_module( 
+    input [254:0] in,
+    output [7:0] out );
+    always@(*)begin
+        out=0;
+        for (integer i=0;i<$bits(in);i++)
+            out=out + in[i];
+    end
+
+endmodule
