686b404b68eaa32736a8f248e6dfa7bc7206c0a5
ADITYA S M <smadityasm@gmail.com>
2025-07-25 11:55:30 +0530

    Create SFSM3(S).v

---
+++ sequential_logic/Finite_State_Machine/SFSM3(S).v
@@ -0,0 +1,35 @@
+module top_module(
+    input clk,
+    input in,
+    input reset,
+    output out);
+    parameter a=2'b00,b=2'b01,c=2'b10,d=2'b11;
+    reg[1:0] state,next_state;
+    always@(*)begin
+        case(state)
+            a:
+                next_state = (in)?b:a;
+            b:
+                next_state = (in)?b:c;
+            c:
+                next_state = (in)?d:a;
+            d:
+                next_state = (in)?b:c;
+            default:
+                next_state = a;
+        endcase
+    end
+    always@(posedge clk)begin
+    
+    if(reset)
+        state <= a;
+    else
+        state <= next_state;
+    end
+    
+        
+
+
+    assign out = (state==d);
+
+endmodule
